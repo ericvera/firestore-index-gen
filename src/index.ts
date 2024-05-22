5f596940deca35042ec472b4ae7918decb82bd82
Eric Vera <6858503+ericvera@users.noreply.github.com>
2024-05-22 14:23:28 -0400

    fix: use process.cwd instead of import.meta.url

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 #! /usr/bin/env node
 import { findUp } from 'find-up'
-import { fileURLToPath } from 'node:url'
 
 const indexPath = await findUp('firebase.index.json')
 
 console.log('Hello!')
-console.log('@:', fileURLToPath(import.meta.url))
+console.log('process.cwd()', process.cwd())
 console.log('indexPath:', indexPath)
 console.log('Bye!')

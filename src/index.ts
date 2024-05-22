fecf38c49316a278c72629094cd5086342faad46
Eric Vera <6858503+ericvera@users.noreply.github.com>
2024-05-22 14:18:37 -0400

    fix: __dirname is not defined in ES modules

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 #! /usr/bin/env node
 import { findUp } from 'find-up'
+import { fileURLToPath } from 'node:url'
 
 const indexPath = findUp('firebase.index.json')
 
 console.log('Hello!')
-console.log('@:', __dirname)
+console.log('@:', fileURLToPath(import.meta.url))
 console.log('indexPath:', indexPath)
 console.log('Bye!')

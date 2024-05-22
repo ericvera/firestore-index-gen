9081ddbf870e2d4f1235a131c82082e93351e55f
Eric Vera <6858503+ericvera@users.noreply.github.com>
2024-05-22 14:13:53 -0400

    feat: find index path

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 #! /usr/bin/env node
+import { findUp } from 'find-up'
+
+const indexPath = findUp('firebase.index.json')
 
 console.log('Hello!')
 console.log('@:', __dirname)
+console.log('indexPath:', indexPath)
 console.log('Bye!')
